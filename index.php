764de4ba3318cd4047e88b43664a2b3f90ea72af
P10140\10253 <pwkacc.yie@gmail.com>
2021-10-26 17:26:54 +0800

    final

--- index.php
+++ index.php
@@ -1,10 +1,13 @@
 <?php
 require './vendor/autoload.php';
 
+// pwk-yie]\test為我製作的套件，利用pakegit的方式。composer require進來
 use code\test1;
 use code\test2;
 use code\test3;
 use code\test4;
+
+
 
 echo test1\test1_2021::info();
 echo test2\test2_2021::info();
