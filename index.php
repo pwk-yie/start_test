950786fd621c0c2d01e79181ea189605fd547ddc
P10140\10253 <pwkacc.yie@gmail.com>
2021-10-26 17:13:08 +0800

    final

--- index.php
+++ index.php
@@ -1,7 +1,12 @@
 <?php
-require 'vendor/autoload.php';
+require './vendor/autoload.php';
 
-use test\src\test;
+use code\test1;
+use code\test2;
+use code\test3;
+use code\test4;
 
-
-echo test\test1_1::info();
+echo test1\test1_2021::info();
+echo test2\test2_2021::info();
+echo test3\test3_2021::info();
+echo test4\test4_2021::info();
